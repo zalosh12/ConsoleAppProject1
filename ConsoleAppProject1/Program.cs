b444dfa1939ad127ba371261c778dcdc8f09b517
Elazar <e0548448478@gmail.com>
2025-05-12 16:58:49 +0300

    Functions of menu

--- ConsoleAppProject1/Program.cs
+++ ConsoleAppProject1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ConsoleAppProject1
 {
@@ -11,20 +12,119 @@ namespace ConsoleAppProject1
         static List<int> Series = new List<int>();
         public static void Main(string[] args)
         {
+            bool IsValid = ValidateNums(args);
+            ManageMenu(IsValid);
            
+
+          
         }
- 
 
 
-        public static void DisplayMenu() { }
 
 
-        public static void InputUser() { }
-
-        static bool ValidateNums(string[] input) { }
 
 
-        public static void DisplayInOrder(int[] numbers) { }
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("a. Input a series");
+            Console.WriteLine("b. Display in order");
+            Console.WriteLine("c. Display in reverse order");
+            Console.WriteLine("d. Display max number");
+            Console.WriteLine("e. Display min number");
+            Console.WriteLine("f. Display average");
+            chooseOption();
+        }
+
+        public static void ManageMenu(bool Valid)
+        {
+            if (Valid)
+            {
+                DisplayMenu();
+            }
+            else
+            {
+                {   Console.WriteLine("Invalid input. Please enter a valid number.");
+                    InputUser();
+                }
+            }
+        }
+
+        static void chooseOption()
+        {
+            bool exit = false;
+            Console.WriteLine("Please choose an option:");
+            string choice = Console.ReadLine();
+            do
+            {
+                switch (choice)
+                {
+                    case "a":
+                        InputUser();
+                        break;
+                    case "b":
+                        DisplayInOrder(Series.ToArray());
+                        break;
+                    case "c":
+                        DisplayInReverse(Series.ToArray());
+                        break;
+                    case "d":
+                        DisplayMax(Series.ToArray());
+                        break;
+                    case "e":
+                        DisplayMin(Series.ToArray());
+                        break;
+                    case "f":
+                        DisplayAverage(Series.ToArray());
+                        break;
+                    case "g":
+                        DisplayNumsCount(Series.ToArray());
+                        break;
+                    case "h":
+                        DisplaySum(Series.ToArray());
+                        break;
+                    case "i":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+
+                }
+                DisplayMenu();
+            } while (!exit);
+        }
+
+
+        public static void InputUser() {
+            Console.WriteLine("Please enter a series of numbers separated by spaces:");
+            string input = Console.ReadLine();
+            ValidateNums(input.Split(' '));
+        }
+        
+
+        static bool ValidateNums(string[] input) {
+            if (input.Length < 3)
+                return false;
+            int[] nums = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!int.TryParse(input[i], out nums[i]))
+                {
+                    return false;
+                }
+            }
+            Series = nums.ToList();
+            return true;
+
+        }
+
+
+        public static void DisplayInOrder(int[] numbers) {
+            foreach (int i in numbers) {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        } 
 
 
         public static void DisplayInReverse(int[] numbers) { }
