cb7902d1896e1cdf4a2c86a183756c4b41f5f345
Elazar <e0548448478@gmail.com>
2025-05-12 15:48:26 +0300

    Functions Signaturs Added

--- ConsoleAppProject1/Program.cs
+++ ConsoleAppProject1/Program.cs
@@ -8,7 +8,44 @@ namespace ConsoleAppProject1
 {
     internal class Program
     {
+        static List<int> Series = new List<int>();
+        public static void Main(string[] args)
+        {
+           
+        }
+ 
 
 
+        public static void DisplayMenu() { }
+
+
+        public static void InputUser() { }
+
+        static bool ValidateNums(string[] input) { }
+
+
+        public static void DisplayInOrder(int[] numbers) { }
+
+
+        public static void DisplayInReverse(int[] numbers) { }
+
+        public static void DisplaySorted(int[] numbers) { }
+
+
+        public static void DisplayMax(int[] numbers) { }
+
+
+        public static void DisplayMin(int[] numbers) { }
+
+
+        public static void DisplayAverage(int[] numbers) { }
+
+
+        public static void DisplayNumsCount(int[] numbers) { }
+
+
+        public static void DisplaySum(int[] numbers) { }
     }
+
+    
 }
